cb2d29719abeb6748a23696cdc9086e2861bd70e
Nils Bonnet <nilsbonnet25@gmail.com>
2025-06-05 15:46:57 +0200

    nice

--- module04/ex00/srcs/main.cpp
+++ module04/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nbonnet <nbonnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 17:52:24 by nbonnet           #+#    #+#             */
-/*   Updated: 2025/06/05 15:32:10 by nbonnet          ###   ########.fr       */
+/*   Updated: 2025/06/05 15:46:41 by nbonnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,38 +33,11 @@ int main()
     wrongCat->makeSound();
     wrongMeta->makeSound();
 
-    Animal* copyAnimal = new Animal();
-    *copyAnimal = *meta;
-    std::cout << copyAnimal->getType() << " " << std::endl;
-    copyAnimal->makeSound();
-
-    Cat* copyCat = new Cat();
-    *copyCat = *new Cat();
-    std::cout << copyCat->getType() << " " << std::endl;
-    copyCat->makeSound();
-
-    Dog* assignedDog = new Dog();
-    *assignedDog = *new Dog();
-    std::cout << assignedDog->getType() << " " << std::endl;
-    assignedDog->makeSound();
-
-    WrongCat* copyWrongCat = new WrongCat();
-    *copyWrongCat = *new WrongCat();
-    std::cout << copyWrongCat->getType() << " " << std::endl;
-    copyWrongCat->makeSound();
-
     delete meta;
     delete j;
     delete i;
     delete wrongMeta;
     delete wrongCat;
-    delete copyAnimal;
-    delete copyCat;
-    delete new Cat();
-    delete assignedDog;
-    delete new Dog();
-    delete copyWrongCat;
-    delete new WrongCat();
 
     return 0;
 }
