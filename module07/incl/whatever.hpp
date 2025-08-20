1b3c7df05d3349e947df4890a41cbeb62a776a31
Nils <nils@example.com>
2025-08-20 17:21:41 +0200

    pushhh

---
+++ module07/incl/whatever.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbonnet <nbonnet@student.42.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/20 16:15:48 by nbonnet           #+#    #+#             */
+/*   Updated: 2025/08/20 16:43:20 by nbonnet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+
+template <class T>
+void swap(T& a, T& b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <class T>
+T& min(T& a, T& b) {
+    if (a < b)
+        return a;
+    return b;       
+}
+
+template <class T>
+T& max(T& a, T& b) {
+    if (a > b)
+        return a;
+    return b;
+}
