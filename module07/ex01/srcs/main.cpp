a93f1067cd523d74c3e0d25fc84a5224a35796fb
Nils <nils@example.com>
2025-08-20 18:17:58 +0200

    dadad

---
+++ module07/ex01/srcs/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbonnet <nbonnet@student.42.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/20 16:15:43 by nbonnet           #+#    #+#             */
+/*   Updated: 2025/08/20 18:17:47 by nbonnet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incl/Iter.hpp"
+
+template <class T>
+void printElement(const T& elem) {
+    std::cout << elem << std::endl;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    size_t len = 5;
+
+    iter(arr, len, printElement<int>);
+    
+    const char carr[] = {'a', 'b', 'c'};
+    iter(carr, 3, printElement<char>);
+
+    return 0;
+}
