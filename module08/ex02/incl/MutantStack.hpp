c3deb09c47a9dba0f481c377dfaf6d9c43b53cea
Nils Bonnet <nilsbonnet25@gmail.com>
2025-09-30 17:03:07 +0200

    -update

--- module08/ex02/incl/MutantStack.hpp
+++ module08/ex02/incl/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: nbonnet <nbonnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 17:38:11 by nbonnet           #+#    #+#             */
-/*   Updated: 2025/09/17 19:09:33 by nbonnet          ###   ########.fr       */
+/*   Updated: 2025/09/30 17:02:57 by nbonnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,20 @@
 template <typename T>
 class MutantStack : public std::stack<T> {
     public:
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        
         iterator begin();
         iterator end(); 
-
-        const_iterator begin() const;
-        const_iterator end() const; 
 };
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin()
+{
+    return (this->c.begin());
+}
+
+template <typename T>
+typename MutantStack<T>::iterator  MutantStack<T>::end()
+{
+    return (this->c.end());
+}
