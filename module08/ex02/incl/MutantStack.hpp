00b9a57936f48fb6043f05ddc33ad841c7155921
Nils Bonnet <nilsbonnet25@gmail.com>
2025-09-30 17:25:23 +0200

    nice

--- module08/ex02/incl/MutantStack.hpp
+++ module08/ex02/incl/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: nbonnet <nbonnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 17:38:11 by nbonnet           #+#    #+#             */
-/*   Updated: 2025/09/30 17:02:57 by nbonnet          ###   ########.fr       */
+/*   Updated: 2025/09/30 17:16:18 by nbonnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,23 @@
 #include <iostream>
 #include <stack>
 
-template <typename T>
-class MutantStack : public std::stack<T> {
+template <typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container> {
     public:
-        typedef typename std::stack<T>::container_type::iterator iterator;
+        typedef typename std::stack<T, Container>::container_type::iterator iterator;
         
         iterator begin();
         iterator end(); 
 };
 
-template <typename T>
-typename MutantStack<T>::iterator MutantStack<T>::begin()
+template <typename T, typename Container>
+typename MutantStack<T, Container>::iterator MutantStack<T, Container>::begin()
 {
     return (this->c.begin());
 }
 
-template <typename T>
-typename MutantStack<T>::iterator  MutantStack<T>::end()
+template <typename T, typename Container>
+typename MutantStack<T, Container>::iterator MutantStack<T, Container>::end()
 {
     return (this->c.end());
 }
