e36ce81af892b67c6294bb2d86bfe4a16f23d1d7
Nils Bonnet <nilsbonnet25@gmail.com>
2025-10-02 17:57:10 +0200

    update

--- module09/ex00/incl/BitcoinExchange.hpp
+++ module09/ex00/incl/BitcoinExchange.hpp
@@ -6,10 +6,13 @@
 /*   By: nbonnet <nbonnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 15:09:32 by nbonnet           #+#    #+#             */
-/*   Updated: 2025/10/02 15:09:48 by nbonnet          ###   ########.fr       */
+/*   Updated: 2025/10/02 17:48:23 by nbonnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
-
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <limits>
---
+++ module09/ex00/srcs/main.cpp
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbonnet <nbonnet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/02 15:09:28 by nbonnet           #+#    #+#             */
+/*   Updated: 2025/10/02 17:56:37 by nbonnet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incl/BitcoinExchange.hpp"
+
+bool is_valid_format(std::string line) {
+    int i = 0;
+    while ( i < 4)
+    {
+        if (!isdigit(line[i]))
+        {
+            std::cout << "Error: bad input => " << line << std::endl;
+            return false;
+        }
+        i++;
+    }
+    if (line[i] != '-')
+    {
+        std::cout << "Error: bad input => " << line << std::endl;
+        return false;
+    }
+    i++;
+    while ( i < 7)
+    {
+        if (!isdigit(line[i]))
+        {
+            std::cout << "Error: bad input => " << line << std::endl;
+            return false;
+        }
+        i++;
+    }
+    if (line[i] != '-')
+    {
+        std::cout << "Error: bad input => " << line << std::endl;
+        return false;
+    }
+    i++;
+    while ( i < 10)
+    {
+        if (!isdigit(line[i]))
+        {
+            std::cout << "Error: bad input => " << line << std::endl;
+            return false;
+        }
+        i++;
+    }
+    if (line[i] != ' ' && line[i + 1] != '|' && line[i + 2] != ' ')
+    {
+        std::cout << "Error: bad input => " << line << std::endl;
+        return false;
+    }
+    i += 2;
+    double value;
+    value = std::atof((line.substr(i ,line.size() - i).c_str()));
+    if (value < 0)
+    {
+        std::cout << "Error: not a positive number." << std::endl;
+        return false;
+    }
+    if (value > 2147483647)
+    {
+        std::cout << "Error: too large number." << std::endl;
+        return false;
+    }
+    std::cout << value << std::endl;
+    return true;
+}
+
+
+int main (int ac, char **av) {
+    if (ac != 2)
+    {
+        std::cout << "Error: need one argument" << std::endl;
+        return 1;
+    }
+    std::ifstream file;
+    file.open (av[1]);
+    if (!file.is_open())
+    {
+        std::cout << "Error: can't open file '" << av[1] << "'" << std::endl;
+        return 1;
+    }
+    std::string line;
+    while (getline(file, line))
+    {
+        is_valid_format(line); 
+    }
+
+    return 0;
+}
