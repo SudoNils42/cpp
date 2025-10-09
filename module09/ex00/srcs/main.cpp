9c981d929388c5bae9ac318e088e3b1145d623c6
Nils Bonnet <nilsbonnet25@gmail.com>
2025-10-09 20:01:20 +0200

    upate

--- module09/ex00/srcs/main.cpp
+++ module09/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nbonnet <nbonnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 15:09:28 by nbonnet           #+#    #+#             */
-/*   Updated: 2025/10/02 18:37:29 by nbonnet          ###   ########.fr       */
+/*   Updated: 2025/10/09 19:57:31 by nbonnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 bool is_valid_format(std::string line) {
     int i = 0;
-    int year;
-    int month;
-    int day;
+    int year = 0;
+    int month = 0;
+    int day = 0;
     while (i < 4)
     {
         if (!isdigit(line[i]))
@@ -42,7 +42,7 @@ bool is_valid_format(std::string line) {
         }
         i++;
     }
-    month = atoi((line.substr(5, 7)).c_str());
+    month = atoi((line.substr(5, 2)).c_str());
     if (line[i] != '-')
     {
         std::cout << "Error: bad input => " << line << std::endl;
@@ -58,7 +58,7 @@ bool is_valid_format(std::string line) {
         }
         i++;
     }
-    day = atoi((line.substr(8, 10)).c_str());
+    day = atoi((line.substr(8, 2)).c_str());
     if (line[i] != ' ' || line[i + 1] != '|' || line[i + 2] != ' ')
     {
         std::cout << "Error: bad input => " << line << std::endl;
@@ -95,6 +95,49 @@ bool is_valid_format(std::string line) {
             flag = 1;
         i++;
     }
+    if (year < 2009 || year > 2025)
+    {
+        std::cout << "Error: bad date." << std::endl;
+        return false;
+    }
+    if (month < 1 || month > 12)
+    {
+        std::cout << "Error: bad date." << std::endl;
+        return false;
+    }
+    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+    {
+        if (day < 1 || day > 31)
+        {
+            std::cout << "Error: bad date." << std::endl;
+            return false;
+        }
+    }
+    if (month == 4 || month == 6 || month == 9 || month == 11)
+    {
+        if (day < 1 || day > 30)
+        {
+            std::cout << "Error: bad date." << std::endl;
+            return false;
+        }
+    }
+    if (month == 2)
+    {
+        if (year % 4 == 0)
+        {
+            if (day < 1 || day > 29)
+            {
+                std::cout << "Error: bad date." << std::endl;
+                return false;
+            }
+
+        }
+        else if (day < 1 || day > 28)
+        {
+            std::cout << "Error: bad date." << std::endl;
+            return false;
+        }
+    } 
     std::cout << year << "-" << month << "-" << day << " => " << value << " = " << std::endl;
     return true;
 }
