793e448778a32bad8c30554afb639412d7b2ffd1
Nils Bonnet <nilsbonnet25@gmail.com>
2025-10-30 14:06:48 +0100

    update

--- module09/ex01/incl/RPN.hpp
+++ module09/ex01/incl/RPN.hpp
@@ -6,12 +6,12 @@
 /*   By: nbonnet <nbonnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 16:11:04 by nbonnet           #+#    #+#             */
-/*   Updated: 2025/10/23 15:17:58 by nbonnet          ###   ########.fr       */
+/*   Updated: 2025/10/30 14:01:45 by nbonnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-#include <deque>
+#include <stack>
 #include <string>
 #include <iostream>
 #include <stdexcept>
@@ -29,60 +29,3 @@ class RPN {
         void parse_input(std::string input);
 };
 
-template <typename T>
-void calculation(std::string input) {
-    std::deque<double> stack;
-    int i = 0;
-    double last = 0;
-    double penultimate = 0;
-    
-    while (input[i])
-    {
-        if (std::isdigit(input[i]))
-            stack.push_back(input[i] - '0');
-        else if (input[i] == '+')
-        {
-            if (stack.size() < 2)
-                throw "Error: wrong expression";
-            last = stack.back();
-            stack.pop_back();
-            penultimate = stack.back();
-            stack.pop_back();
-            stack.push_back(penultimate + last);
-        }
-        else if (input[i] == '-')
-        {
-            if (stack.size() < 2)
-                throw "Error: wrong expression";
-            last = stack.back();
-            stack.pop_back();
-            penultimate = stack.back();
-            stack.pop_back();
-            stack.push_back(penultimate - last);
-        }
-        else if (input[i] == '/')
-        {
-            if (stack.size() < 2)
-                throw "Error: wrong expression";
-            last = stack.back();
-            if (last == 0)
-                throw "Error: / by 0 is impossible";
-            stack.pop_back();
-            penultimate = stack.back();
-            stack.pop_back();
-            stack.push_back(penultimate / last);
-        }
-        else if (input[i] == '*')
-        {
-            if (stack.size() < 2)
-                throw "Error: wrong expression";
-            last = stack.back();
-            stack.pop_back();
-            penultimate = stack.back();
-            stack.pop_back();
-            stack.push_back(penultimate * last);
-        }
-        i++;
-    }
-    std::cout << stack.back() << std::endl;
-}
--- module09/ex01/srcs/main.cpp
+++ module09/ex01/srcs/main.cpp
@@ -6,11 +6,69 @@
 /*   By: nbonnet <nbonnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 16:54:13 by nbonnet           #+#    #+#             */
-/*   Updated: 2025/10/23 15:19:10 by nbonnet          ###   ########.fr       */
+/*   Updated: 2025/10/30 14:02:59 by nbonnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/RPN.hpp"
+
+void calculation(std::string input) {
+    std::stack<double> stack;
+    int i = 0;
+    double last = 0;
+    double penultimate = 0;
+    
+    while (input[i])
+    {
+        if (std::isdigit(input[i]))
+            stack.push(input[i] - '0');
+        else if (input[i] == '+')
+        {
+            if (stack.size() < 2)
+                throw "Error: wrong expression";
+            last = stack.top();
+            stack.pop();
+            penultimate = stack.top();
+            stack.pop();
+            stack.push(penultimate + last);
+        }
+        else if (input[i] == '-')
+        {
+            if (stack.size() < 2)
+                throw "Error: wrong expression";
+            last = stack.top();
+            stack.pop();
+            penultimate = stack.top();
+            stack.pop();
+            stack.push(penultimate - last);
+        }
+        else if (input[i] == '/')
+        {
+            if (stack.size() < 2)
+                throw "Error: wrong expression";
+            last = stack.top();
+            if (last == 0)
+                throw "Error: / by 0 is impossible";
+            stack.pop();
+            penultimate = stack.top();
+            stack.pop();
+            stack.push(penultimate / last);
+        }
+        else if (input[i] == '*')
+        {
+            if (stack.size() < 2)
+                throw "Error: wrong expression";
+            last = stack.top();
+            stack.pop();
+            penultimate = stack.top();
+            stack.pop();
+            stack.push(penultimate * last);
+        }
+        i++;
+    }
+    std::cout << stack.top() << std::endl;
+}
+
 
 int main (int ac, char **av) {
     if (ac != 2)
@@ -21,7 +79,7 @@ int main (int ac, char **av) {
     RPN rpn;
     try {
         rpn.parse_input(std::string(av[1]));
-        calculation<double>(std::string(av[1]));
+        calculation(std::string(av[1]));
     } catch (const char* e) {
         std::cerr << e << std::endl; 
     }
