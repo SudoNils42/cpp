d7fc4b8aa3f985d7d38e3655484a300ccab54140
Nils Bonnet <nilsbonnet25@gmail.com>
2025-10-30 14:24:42 +0100

    pourkim

--- module09/ex02/incl/PmergeMe.hpp
+++ module09/ex02/incl/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: nbonnet <nbonnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 15:30:04 by nbonnet           #+#    #+#             */
-/*   Updated: 2025/10/29 19:22:48 by nbonnet          ###   ########.fr       */
+/*   Updated: 2025/10/30 14:16:22 by nbonnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,17 +44,20 @@ template <typename Container>
 void ford_johnson(Container &container) {
     if (container.size() <= 1)
         return;
-
     Container main;
     Container pend;
-    
+   
     int odd = -1;
     if (container.size() % 2 == 1)
     {
         odd = container.back();
         container.pop_back();
     }
-    
+   
+    std::cout << "DEBUG: Taille initiale: " << container.size() << std::endl;
+    if (odd != -1)
+        std::cout << "DEBUG: Element impair extrait: " << odd << std::endl;
+
     typename Container::iterator it = container.begin();
     while (it != container.end())
     {
@@ -64,8 +67,23 @@ void ford_johnson(Container &container) {
         main.push_back(std::min(a, b));
         pend.push_back(std::max(a, b));
         it++;
+
+        std::cout << "DEBUG: Paire (" << a << ", " << b 
+                  << ") -> main: " << std::min(a, b) 
+                  << ", pend: " << std::max(a, b) << std::endl;
     }
+
+    std::cout << "DEBUG: Appel recursif sur main (taille: " << main.size() << ")" << std::endl;
+
     ford_johnson(main);
+    std::cout << "DEBUG: Main trie: ";
+    for (typename Container::iterator i = main.begin(); i != main.end(); ++i)
+        std::cout << *i << " ";
+    std::cout << std::endl;
+    std::cout << "DEBUG: Pend en attente: ";
+    for (typename Container::iterator i = pend.begin(); i != pend.end(); ++i)
+        std::cout << *i << " ";
+    std::cout << std::endl;
 
     int n = 1;
     while (!pend.empty())
@@ -73,22 +91,43 @@ void ford_johnson(Container &container) {
         int j = jacobsthal(n);
         if (j >= static_cast<int>(pend.size()))
             break;
+
+        std::cout << "DEBUG: Jacobsthal(" << n << ") = " << j 
+                  << " -> insertion de pend[" << j << "]" << std::endl;
+
         typename Container::iterator it = pend.begin();
         for (int i = 0; i < j; i++)
             it++;
         int val = *it;
         pend.erase(it);
+
+        std::cout << "DEBUG: Insertion de " << val << " dans main" << std::endl;
+
         main.insert((std::lower_bound(main.begin(), main.end(), val)), val);
+
         n++;
     }
     if (odd != -1)
         pend.push_back(odd);
 
+    std::cout << "DEBUG: Insertion finale des elements restants (pend): ";
+    for (typename Container::iterator i = pend.begin(); i != pend.end(); ++i)
+        std::cout << *i << " ";
+    std::cout << std::endl;
+
     while (!pend.empty())
     {
         int val = pend.back();
         pend.pop_back();
+
+        std::cout << "DEBUG: Insertion finale de " << val << std::endl;
+
         main.insert((std::lower_bound(main.begin(), main.end(), val)), val);
     }
     container = main;
+
+    std::cout << "DEBUG: Conteneur trie: ";
+    for (typename Container::iterator i = container.begin(); i != container.end(); ++i)
+        std::cout << *i << " ";
+    std::cout << std::endl;
 }
